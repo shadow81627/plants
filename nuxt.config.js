8bdd852952803cffd52498b2e4a2ecc7578188be
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-08-07 20:39:03 +1000

    chore: id in file names

--- nuxt.config.js
+++ nuxt.config.js
@@ -160,6 +160,10 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {
+    filenames: {
+      chunk: ({ isDev }) =>
+        isDev ? '[name].js' : 'chunks/[id].[contenthash].js',
+    },
     transpile: ['lodash-es'],
   },
 }
