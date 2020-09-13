0b2ea07b4b250c76843276a908d9d1cc7cefc26c
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-09-13 15:48:25 +1000

    chore(lighthouse): add urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,18 @@
 module.exports = {
   ci: {
     collect: {
+      url: [
+        '/',
+        '/404.html',
+        '/species',
+        '/species/Creeping%20boobialla%20(Myoporum%20parvifolium)',
+        '/nurseries',
+        '/nurseries/City%20Farm%20Nursery%20(Open%20Thursday%20to%20Sunday)',
+        '/sources',
+        '/releases',
+      ],
       staticDistDir: './dist',
-      maxAutodiscoverUrls: 0,
+      numberOfRuns: 5,
     },
     upload: {
       target: 'temporary-public-storage',
