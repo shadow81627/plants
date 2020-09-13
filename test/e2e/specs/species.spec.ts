47f2b80953475ca9640c2bc4e1a75797e401e652
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-09-13 14:58:51 +1000

    test(e2e): codecept

---
+++ test/e2e/specs/species.spec.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+
+Feature('Species')
+
+Scenario('Find a Species', async (I) => {
+  I.amOnPage('/species')
+  I.click(
+    '#app > div > main > div > div > div.v-data-iterator > div > div:nth-child(1) > div > a'
+  )
+
+  I.saveScreenshot('species_list.png')
+  await I.seeVisualDiff('species_list.png')
+
+  I.amOnPage('/species/Creeping%20boobialla%20(Myoporum%20parvifolium)')
+  I.saveScreenshot('species_details.png')
+  await I.seeVisualDiff('species_details.png')
+})
